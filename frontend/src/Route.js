8b10cc2dd150a1a345a37d7e779d77c6ead6a74e
Ben Tan <benjaminthtan@gmail.com>
2023-05-02 20:05:56 +0800

    add last few pages

--- frontend/src/Route.js
+++ frontend/src/Route.js
@@ -1,9 +1,15 @@
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/SignupPage.jsx";
 import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import BestSellingPage from "./pages/BestSellingPage";
+import FAQPage from "./pages/FAQPage";
 
 export {
     LoginPage,
     SignupPage,
     HomePage,
+    ProductsPage,
+    BestSellingPage,
+    FAQPage,
 };
