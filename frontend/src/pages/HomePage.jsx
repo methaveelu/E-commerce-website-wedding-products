c7afed57e22db4820af236d76ae7c078699d6341
Ben Tan <benjaminthtan@gmail.com>
2023-05-02 17:53:32 +0800

    add Sponsored.jsx and FeaturedProducts.jsx + update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Header from '../components/Layout/Header'
-import Hero from "../components/Layout/Hero"
+import React from 'react';
+import Header from '../components/Layout/Header';
+import Hero from "../components/Layout/Hero";
+import FeaturedProduct from '../components/Route/FeaturedProduct/FeaturedProduct';
+import Sponsored from "../components/Route/Sponsored.jsx";
 
 const HomePage = () => {
   return (
     <div>
         <Header activeHeading={1}/>
         <Hero />
+        <FeaturedProduct />
+        <Sponsored />
     </div>
   )
 }
 
-export default HomePage
+export default HomePage;
