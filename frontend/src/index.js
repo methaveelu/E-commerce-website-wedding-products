b86b6059708d090ff5854f8d53348a3196df913d
Ben Tan <benjaminthtan@gmail.com>
2023-05-02 20:57:52 +0800

    bug fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,9 @@ import App from './App';
 import { Provider } from "react-redux";
 import Store from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
+ReactDOM.render(
   <Provider store={Store}>
     <App />
-  </Provider>
-  // </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
