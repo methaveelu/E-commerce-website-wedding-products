6cfb3db29221fd4b4a4a40b785df1d10c9d96ef0
Yu Jiapeng <yujiapeng0625@gmail.com>
2023-05-04 21:50:38 +0800

    updates

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -18,14 +18,14 @@ router.post("/create-user", upload.single("file"),async (req, res, next) => {
     const userEmail = await User.findOne({ email });
 
     if (userEmail) {
-      // const filename = req.file.filename;
-      // const filePath = `uploads/${filename}`;
-      // fs.unlink(filePath, (err) => {
-      //   if (err) {
-      //     console.log(err);
-      //     res.status(500).json({ message: "Error deleting file" });
-      //   }
-      // });
+      const filename = req.file.filename;
+      const filePath = `uploads/${filename}`;
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ message: "Error deleting file" });
+        }
+      });
       return next(new ErrorHandler("User already exists", 400));
     }
 
