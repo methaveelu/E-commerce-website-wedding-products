8af5f374e0c253e363959091252651916ee57960
Ben Tan <benjaminthtan@gmail.com>
2023-05-06 17:36:10 +0800

    fix redux bugs

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -12,24 +12,6 @@ import { toast } from "react-toastify";
 const Cart = ({ setOpenCart }) => {
   const { cart } = useSelector((state) => state.cart);
 
-  const cartData = [
-    {
-      Name: "SHERRI HILL 51671 DRESS",
-      description: "test",
-      price: 1099,
-    },
-    {
-      Name: "Mid Blue Three-Piece Lazio Suit",
-      description: "test",
-      price: 1099,
-    },
-    {
-      Name: "Dandelion Tassel leather loafers",
-      description: "test",
-      price: 300,
-    },
-  ];
-
   const dispatch = useDispatch();
 
   const removeFromCartHandler = (data) => {
@@ -48,7 +30,7 @@ const Cart = ({ setOpenCart }) => {
   return (
     <div className="fixed top-0 left-0 w-full bg-[#0000004b] h-screen z-10">
       <div className="fixed top-0 right-0 h-full w-[80%] 800px:w-[25%] bg-gray-200 flex flex-col overflow-y-scroll justify-between shadow-sm">
-        {cartData && cartData.length === 0 ? (
+        {cart && cart.length === 0 ? (
           <div className="w-full h-screen flex items-center justify-center">
             <div className="flex w-full justify-end pt-5 pr-5 fixed top-3 right-3">
               <RxCross1
@@ -69,19 +51,19 @@ const Cart = ({ setOpenCart }) => {
                   onClick={() => setOpenCart(false)}
                 />
               </div>
-              {/* Item length */}
+              {/* item length */}
               <div className={`${styles.normalFlex} p-4`}>
                 <IoBagHandleOutline size={25} />
                 <h5 className="pl-2 text-[20px] font-[500]">
-                  {cartData && cartData.length} items
+                  {cart && cart.length} items
                 </h5>
               </div>
 
-              {/* cart Single Items */}
+              {/* CartSingle items */}
               <br />
               <div className="w-full border-t">
-                {cartData &&
-                  cartData.map((i, index) => (
+                {cart &&
+                  cart.map((i, index) => (
                     <CartSingle
                       key={index}
                       data={i}
@@ -99,7 +81,7 @@ const Cart = ({ setOpenCart }) => {
                   className={`h-[45px] flex items-center justify-center w-[100%] bg-[#e44343] rounded-[5px]`}
                 >
                   <h1 className="text-[#fff] text-[18px] font-[600]">
-                    Checkout Now (SGD$1000)
+                    Checkout Now
                   </h1>
                 </div>
               </Link>
