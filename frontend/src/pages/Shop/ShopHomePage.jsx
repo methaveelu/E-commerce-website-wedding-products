c442a594afe8d1e14b47cf5bcace04697038c9f3
methaveelu <110245885+methaveelu@users.noreply.github.com>
2023-05-07 18:12:44 +0800

    seller-dashboardv1

--- frontend/src/pages/Shop/ShopHomePage.jsx
+++ frontend/src/pages/Shop/ShopHomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../../styles/styles";
 import ShopInfo from "../../components/Shop/ShopInfo";
-import ShopProfileData from "../../components/Shop/ShopProfileData";
+// import ShopProfileData from "../../components/Shop/ShopProfileData";
 
 const ShopHomePage = () => {
   return (
@@ -10,9 +10,9 @@ const ShopHomePage = () => {
         <div className="w-[25%] bg-[#fff] rounded-[4px] shadow-sm overflow-y-scroll h-[90vh] sticky top-10 left-0 z-10">
           <ShopInfo isOwner={true} />
         </div>
-        <div className="w-[72%] rounded-[4px]">
+        {/* <div className="w-[72%] rounded-[4px]">
           <ShopProfileData isOwner={true} />
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- frontend/src/routes/ProtectedRoute.js
+++ frontend/src/routes/ProtectedRoute.js
@@ -3,6 +3,7 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const { loading, isAuthenticated } = useSelector((state) => state.user);
+  
   if (loading === false) {
     if (!isAuthenticated) {
       return <Navigate to="/login" replace />;
