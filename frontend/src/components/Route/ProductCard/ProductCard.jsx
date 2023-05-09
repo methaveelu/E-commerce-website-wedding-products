f10fed08e93eb4bb4b020a389cc91d1e9ba2dc90
Yu Jiapeng <yujiapeng0625@gmail.com>
2023-05-09 22:43:27 +0800

    updates

--- frontend/src/components/Route/ProductCard/ProductCard.jsx
+++ frontend/src/components/Route/ProductCard/ProductCard.jsx
@@ -77,11 +77,6 @@ console.log("data",data);
         </Link>
         <Link to={`${`/product/${data._id}`}`}>
 
-        <Link to={`/shop/preview/${data?.shop._id}`}>
-          <h5 className={`${styles.shop_name}`}>{data.shop.name}</h5>
-        </Link>
-        <Link
-          to={`${`/product/${data._id}`}`}>
           <h4 className="pb-3 font-[500]">
             {data.name.length > 40 ? data.name.slice(0, 40) + "..." : data.name}
           </h4>
